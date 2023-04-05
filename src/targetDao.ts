e4d5057d0ba844eb248b2618bace16d0373b544f
bitbeckers <code@bitbeckers.com>
2023-04-05 15:23:46 +0200

    feat(dao): added dao to targetDao.ts

--- src/targetDao.ts
+++ src/targetDao.ts
@@ -7,14 +7,9 @@ export const TARGET_DAO: {
     CHAIN_ID: ValidNetwork;
   };
 } = {
-  "0xf6538c07324f59b3ba685d86393c65dce9676c70": {
-    ADDRESS: "0xf6538c07324f59b3ba685d86393c65dce9676c70",
-    SAFE_ADDRESS: "0xb64b12c4e68310fc222580dea1c86d202310f343",
+  "0x0c495cb5b5f2313e84c4a1a584ac27f42fd78d29": {
+    ADDRESS: "0x0c495cb5b5f2313e84c4a1a584ac27f42fd78d29",
+    SAFE_ADDRESS: "0xc0f3a7737cb8338c7fa0de5bc23acb0060d9f642",
     CHAIN_ID: "0x5",
-  },
-  "0xf844b98df9ccdfbe5d460d0d7bdca232cf9da923": {
-    ADDRESS: "0xf844b98df9ccdfbe5d460d0d7bdca232cf9da923",
-    SAFE_ADDRESS: "0xeb0dc703b854791914f30b5a73dd04d8d22a9aff",
-    CHAIN_ID: "0x1",
   },
 };
