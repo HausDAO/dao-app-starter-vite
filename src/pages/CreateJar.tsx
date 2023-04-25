b514baf81c91d052458d8a2ae7c24381eea7253d
dekanbro <dekanbrown@odyssy.io>
2023-04-25 15:56:52 -0600

    new jar select page

--- src/pages/CreateJar.tsx
+++ src/pages/CreateJar.tsx
@@ -4,12 +4,31 @@ import { MolochFields } from "@daohaus/moloch-v3-fields";
 import { APP_FORM } from "../legos/forms";
 import { TARGET_DAO } from "../targetDao";
 import { AppFieldLookup } from "../legos/fieldConfig";
+import { Button, H2, SingleColumnLayout } from "@daohaus/ui";
 
 export const CreateJar = () => {
+  // set up different pages and routes for these forms
+  return (
+    <>
+      <SingleColumnLayout>
+        <H2>Pick A Jar Type</H2>
+
+        <Button style={{ marginTop: "2rem", marginBottom: "2rem" }}>DaoHAUS</Button>
+        <Button disabled={true} style={{ marginTop: "2rem", marginBottom: "2rem" }}>ERC20(coming soon)</Button>
+        <Button disabled={true} style={{ marginTop: "2rem", marginBottom: "2rem" }}>ERC721(coming soon)</Button>
+        <Button disabled={true} style={{ marginTop: "2rem", marginBottom: "2rem" }}>Custom(coming soon)</Button>
+      </SingleColumnLayout>
+    </>
+  );
+};
+
+{
+  /* export const CreateJar = () => {
   return (
     <FormBuilder
       form={APP_FORM.CREATEJAR}
       targetNetwork={TARGET_DAO[import.meta.env.VITE_TARGET_KEY].CHAIN_ID}
     />
   );
-};
+}; */
+}
