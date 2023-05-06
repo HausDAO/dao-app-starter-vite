867d58df0c61897fcd12828b91a3ea62cece2dc5
dekanbro <dekanbrown@odyssy.io>
2023-05-06 15:12:47 -0600

    remove env and current dao

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,21 +1,13 @@
 import { Routes as Router, Route } from "react-router-dom";
-import { FormTest } from "./pages/FormTest";
 import { Home } from "./pages/Home";
 import { LayoutContainer } from "./components/LayoutContainer";
-import Dao from "./pages/Dao";
-import { TARGET_DAO } from "./targetDao";
 
-const routePath = `molochv3/${
-  TARGET_DAO[import.meta.env.VITE_TARGET_KEY].CHAIN_ID
-}/${TARGET_DAO[import.meta.env.VITE_TARGET_KEY].ADDRESS}`;
 
 export const Routes = () => {
   return (
     <Router>
       <Route path="/" element={<LayoutContainer />}>
         <Route index element={<Home />} />
-        <Route path={`${routePath}/formtest`} element={<FormTest />} />
-        <Route path={`${routePath}/dao`} element={<Dao />} />
       </Route>
     </Router>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { DHConnectProvider } from "@daohaus/connect";
 import { HausThemeProvider } from "@daohaus/ui";
 import { Buffer } from "buffer";
 
 import { Routes } from "./Routes";
-import { TARGET_DAO } from "./targetDao";
 
 // This solves an issue when using WalletConnect and intercept Txs to create dao proposals
 // Related open issue: https://github.com/WalletConnect/walletconnect-monorepo/issues/748
 window.Buffer = window.Buffer || Buffer;
 
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HashRouter>
-      <QueryClientProvider client={queryClient}>
-        <DHConnectProvider
-          daoChainId={TARGET_DAO[import.meta.env.VITE_TARGET_KEY].CHAIN_ID}
-        >
+
           <HausThemeProvider>
             <Routes />
           </HausThemeProvider>
-        </DHConnectProvider>
-      </QueryClientProvider>
+
     </HashRouter>
   </React.StrictMode>
 );
